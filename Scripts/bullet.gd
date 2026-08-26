632ea3a789d49ab98487058ad88a5463f28c046f
Gabriel <minegplayer@gmail.com>
2026-08-26 18:41:24 +0100

    Finalized Bullets!!!!!!!!!!!!!!

--- Scripts/bullet.gd
+++ Scripts/bullet.gd
@@ -4,3 +4,8 @@ var speed : float = 20
 
 func _physics_process(delta):
 	position += transform.basis * Vector3(0,0,-speed) * delta
+
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	if body.is_in_group("box"):
+		print("box entered")
